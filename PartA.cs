ded1fc3eebced4346653ba1befb72034d1ecc8e2
Daniel Hoskin <ac122731@avcol.school.nz>
2023-03-17 17:24:16 +1300

    Part A Done
    It Can Enter Their Name, Then Their GPA then Display Their Age and GPA

--- PartA.cs
+++ PartA.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This Program Will ask For Your Age And Current GPA and Prints them the the screen.\n");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Please Enter Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Thread.Sleep(1000);
+
+            Console.WriteLine("\n Please enter Your GPA: ");
+            double gpa = double.Parse(Console.ReadLine());
+            Console.WriteLine("\nAge That Was Entered " +  age);
+            Console.WriteLine("GPA Entered Was: " +  gpa);
+            Thread.Sleep(1000);
+
+            Console.WriteLine("\nGoodBye!");
         }
     }
 }
+
